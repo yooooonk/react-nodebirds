dd361f180a53cb450b7c095c35c827b70f5b3f6c
ouo_yoonk <joyk9251@gmail.com>
2021-01-13 15:45:56 +0900

    이미지 보기, 이미지 슬릭 추가

--- front/compoenets/PostImages.js
+++ front/compoenets/PostImages.js
@@ -1,10 +1,56 @@
 import PropTypes from 'prop-types'
+import {PlusOutlined} from '@ant-design/icons'
+import { useCallback, useState } from 'react';
+import ImagesZoom from './imagesZoom';
 const PostImages = ({images})=>{
+
+    const [showImagesZoom, setShowImagesZoom] = useState(false)
+
+    const onZoom = useCallback(()=>{
+        setShowImagesZoom(true)
+    },[])
+    
+    const onClose = useCallback(()=>{                
+        setShowImagesZoom(false)        
+    },[])
+
+
+    if(images.length === 1){
+        return(
+            <>
+                <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom}/>               
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+            </>
+        )
+    }
+
+    if(images.length === 2){
+        return(
+            <>
+                <img role="presentation" width="50%" src={images[0].src} alt={images[0].src} onClick={onZoom}/>               
+                <img role="presentation" width="50%" src={images[1].src} alt={images[1].src} onClick={onZoom}/>               
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+            </>
+        )
+    }
+
     return (
-        <div>
-            구현중
+        <div style={{display:'flex', alignItems:'center', justifyContent:'center' }}>
+            <img role="presentation" width="50%" src={images[0].src} alt={images[0].src} onClick={onZoom}/>               
+            <div 
+                role="presentation"
+                style={{display:'inline-block', width:'50%', textAlign:'center', verticalAlign:'middle'}}
+                onClick={onZoom}
+            >   
+                <PlusOutlined />
+                <br/>
+                {images.length-1}개 더보기
+            </div>
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
         </div>
     )
+
+    
 };
 
 PostImages.propTypes = {
